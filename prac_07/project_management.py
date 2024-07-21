460c1f49cf7f0b8fcc97c995819617df209c50e3
lizhixing <zhixing.li@my.jcu.edu.au>
2024-07-21 20:36:18 +0800

    prac 7 projectmanagement.py load and save functions are done

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -60,6 +60,32 @@ def main():
 
 
 
+def load_projects(filename):
+    projects = []
+    try:
+        with open(filename, 'r') as file:
+            file.readline()
+            for line_number, line in enumerate(file, start=2):
+                line = line.strip()
+                if line:
+                    try:
+                        name, start_date, priority, cost_estimate, completion_pct = line.split('\t')
+                        projects.append(Project(name, start_date, int(priority), float(cost_estimate), int(completion_pct)))
+                    except ValueError:
+                        print(f"Error in line {line_number}: Invalid format or values.")
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+    return projects
+
+
+def save_projects(filename, projects):
+    with open(filename, 'w') as file:
+        file.write("Name\tStart Date\tPriority\tCost Estimate\tCompletion (%)\n")
+        for project in projects:
+            file.write(
+                f"{project.name}\t{project.start_date.strftime('%d/%m/%Y')}\t{project.priority}\t{project.cost_estimate:.2f}\t{project.completion_pct}\n")
+
+
 
 
 
